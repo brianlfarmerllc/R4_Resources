c727a0aa4c3eeecf76a0c8849b71156b913728bc
Brian Farmer <brianlfarmerllc@gmail.com>
2020-09-29 16:07:30 -0500

    finished setting up the contact form front end design

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.scss";
 import logo from "../../assets/R4 80.png";
 
 const Contact = () => {
+  const [type, setType] = useState(null);
+  const [fileName, setFileName] = useState("Upload Your Resume (optional)");
+
+  function handleChange(e) {
+    const { value } = e.target;
+
+    switch (value) {
+      case "general":
+        setType(null);
+        setFileName("Upload Your Resume (optional)");
+        break;
+      case "company":
+        setType(
+          <div className="company-name">
+            <input
+              type="text"
+              className="company"
+              placeholder="Company Name (optional)"
+              name="company"
+            />
+          </div>
+        )
+        setFileName("Upload Your Resume (optional)");
+        break;
+      case "employee":
+        setType(null);
+        setFileName("Upload Your Resume (optional)");
+        break;
+
+      default:
+        setType(null);
+        setFileName("Upload Your Resume (optional)");
+        break;
+    }
+  }
+
+  function seeFile(e) {
+    const { files } = e.target;
+    if (files[0] === undefined) {
+      return;
+    } else {
+      setFileName(files[0].name);
+    }
+  }
+
   return (
     <section className="contact">
       <div className="top-text">
@@ -15,23 +60,38 @@ const Contact = () => {
         <form action="">
           <div className="question">
             <h3>I'm Looking For</h3>
-            <select name="contactType">
-              <option value="">Answers to General Questions</option>
-              <option value="">Talented Technology Professionals</option>
-              <option value="">Opprotunities With A Great Company</option>
+            <select name="contactType" onChange={handleChange}>
+              <option value="general">Answers to General Questions</option>
+              <option value="company">Talented Technology Professionals</option>
+              <option value="employee">
+                Opportunities With A Great Company
+              </option>
             </select>
           </div>
+          <div className="resume">
+            <input type="file" id="uglybutton" onChange={seeFile} name="file" />
+
+            <label
+              htmlFor="uglybutton"
+              id="uploadText"
+              type="text"
+              name="filename"
+            >
+              {fileName}
+            </label>
+          </div>
+          {type}
           <div className="fullname">
             <input
               type="text"
               className="first"
-              placeholder="First Name"
+              placeholder="* First Name"
               name="first"
             />
             <input
               type="text"
               className="last"
-              placeholder="Last Name"
+              placeholder="* Last Name"
               name="last"
             />
           </div>
@@ -39,13 +99,13 @@ const Contact = () => {
             <input
               type="tel"
               className="phone"
-              placeholder="Phone Number"
+              placeholder="* Phone Number"
               name="email"
             />
             <input
               type="email"
               className="email"
-              placeholder="Email"
+              placeholder="* Email"
               name="email"
             />
           </div>
@@ -53,11 +113,13 @@ const Contact = () => {
             <textarea
               name="message"
               rows="6"
-              placeholder="I'd Like To Know More About..."
+              placeholder="* I'd Like To Know More About..."
             ></textarea>
           </div>
-          <div className="send"></div>
-          <button>Submit</button>
+
+          <div className="send">
+            <button>Submit</button>
+          </div>
         </form>
       </div>
     </section>
