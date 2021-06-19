b0edb54bb73d4f3b95a1155b15ea2ac48d7c2bd7
Brian Farmer <brianlfarmerllc@gmail.com>
2021-06-18 23:24:50 -0500

    clean up after merge ready to push to github

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
+import Footer from "./components/Footer";
 
 function App() {
   return (
